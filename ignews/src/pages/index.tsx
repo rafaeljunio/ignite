ae2f398da6d0904d326557f9d1d6cd71ede9582e
Rafael Junio <rafaeljunio7@gmail.com>
2021-10-07 10:44:44 -0300

    Update

---
+++ ignews/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | Ig.News</title>
+      </Head>
+      <h1>
+        Hello World
+      </h1>
+    </>
+  )
+}
